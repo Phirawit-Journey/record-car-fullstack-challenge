d0b0eee2f558e2722009f6e87d0c543b2ec2ac75
Phirawit Tungjaroenvorrakhun <168079020+Phirawit-Journey@users.noreply.github.com>
2024-07-17 16:37:32 +0700

    Set component folder in react-side

--- crud-react/src/App.js
+++ crud-react/src/App.js
@@ -1,11 +1,24 @@
-import Button from "react-bootstrap/Button";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./components/dashboard/dashboard";
+import NoMatch from "./components/nomatch/noMatch";
+import PostCardata from "./components/postCardata/postCardata";
+import UpdateCardata from "./components/updateCardata/updateCardata";
+import Header from "./components/header/header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Crud</h1>
-      <Button>Click</Button>
-    </div>
+    <>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/car" element={<PostCardata></PostCardata>}></Route>
+        <Route
+          path="/car/:id"
+          element={<UpdateCardata></UpdateCardata>}
+        ></Route>
+        <Route path="*" element={<NoMatch></NoMatch>}></Route>
+      </Routes>
+    </>
   );
 }
 
